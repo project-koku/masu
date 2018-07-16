e687aa47040772a26235e9c560146bf06fc86ee0
Andrew Berglund <aberglun@redhat.com>
2018-07-16 10:37:04 -0400

    93 celery routing (#95)
    * Using a concrete variable assignment for celery in place of a factory function
    
    * Associating tasks directly to the celery app with `@celery.task` in stead of `@shared_task`

--- tests/celery/test_celery.py
+++ tests/celery/test_celery.py
@@ -16,9 +16,13 @@
 #
 """Test the celery functions."""
 
-from celery import Celery
 
-from masu.celery import create_celery
+from celery import Celery
+from celery.app.task import Task
+
+from masu import create_app
+from masu.celery import celery, update_celery_config
+from masu.config import Config
 from tests import MasuTestCase
 
 
@@ -26,6 +30,29 @@ class CeleryTest(MasuTestCase):
     """Test Cases for the Celery functions."""
 
     def test_make_celery(self):
-        """Test that we can get a celery object."""
-        queue = create_celery(self.app)
-        self.assertIsInstance(queue, Celery)
+        """Test that we can get a Celery object."""
+        self.assertIsInstance(celery, Celery)
+
+    def test_update_celery_config(self):
+        """Test that the app config updates our celery instance."""
+        test_celery = Celery(__name__, broker=Config.CELERY_BROKER_URL)
+        test_app = create_app(
+            {
+                'TESTING': True,
+                'SQLALCHEMY_TRACK_MODIFICATIONS': False,
+                'SQLALCHEMY_DATABASE_URI': 'sqlite:///test.db',
+                'SCHEDULE_REPORT_CHECKS': True
+            }
+        )
+
+        self.assertIsNone(test_celery.conf.task_routes)
+        self.assertEqual(test_celery.conf.imports, ())
+        self.assertEqual(test_celery.conf.beat_schedule, {})
+        self.assertEqual(repr(test_celery.Task), repr(Task))
+
+        update_celery_config(test_celery, test_app)
+
+        self.assertIsNotNone(test_celery.conf.task_routes)
+        self.assertNotEqual(test_celery.conf.imports, ())
+        self.assertNotEqual(test_celery.conf.beat_schedule, {})
+        self.assertNotEqual(repr(test_celery.Task), repr(Task))

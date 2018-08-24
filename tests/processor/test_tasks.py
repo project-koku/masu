1e8690d6857ff0ae223af95f9b788bfb6ddc68c8
Douglas Curtis <docurtis@redhat.com>
2018-08-24 10:59:32 -0400

    Avoid re-processing of CUR files (#162)

--- tests/processor/test_tasks.py
+++ tests/processor/test_tasks.py
@@ -220,8 +220,8 @@ class TestProcessorTasks(MasuTestCase):
                                                  mock_started,
                                                  mock_completed):
         """
-        Test that the chained task is called when start timestamp is before
-        end timestamp.
+        Test that the chained task is not called when start timestamp is before
+        end timestamp since both started and completed times are present.
         """
         mock_process_files.delay = Mock()
         mock_get_files.return_value = self.fake_reports
@@ -230,7 +230,7 @@ class TestProcessorTasks(MasuTestCase):
         mock_completed.return_value = self.today
 
         get_report_files(**self.fake_get_report_args)
-        mock_process_files.delay.assert_called()
+        mock_process_files.delay.assert_not_called()
 
     @patch('masu.processor.tasks.ReportStatsDBAccessor.get_last_completed_datetime')
     @patch('masu.processor.tasks.ReportStatsDBAccessor.get_last_started_datetime')
@@ -284,7 +284,8 @@ class TestProcessorTasks(MasuTestCase):
                                                mock_started,
                                                mock_completed):
         """
-        Test that the chained task is called when no end time is set.
+        Test that the chained task is not called when no end time is set since
+        processing is in progress.
         """
         mock_process_files.delay = Mock()
         mock_get_files.return_value = self.fake_reports
@@ -292,7 +293,59 @@ class TestProcessorTasks(MasuTestCase):
         mock_started.return_value = self.today
         mock_completed.return_value = None
         get_report_files(**self.fake_get_report_args)
+        mock_process_files.delay.assert_not_called()
+
+    @patch('masu.processor.tasks.ReportStatsDBAccessor.get_last_completed_datetime')
+    @patch('masu.processor.tasks.ReportStatsDBAccessor.get_last_started_datetime')
+    @patch('masu.processor.tasks._get_report_files')
+    @patch('masu.processor.tasks.process_report_file')
+    @patch('masu.external.date_accessor.DateAccessor.today')
+    def test_get_report_files_timestamps_empty_end_timeout(self,
+                                               mock_date,
+                                               mock_process_files,
+                                               mock_get_files,
+                                               mock_started,
+                                               mock_completed):
+        """
+        Test that the chained task is called when no end time is set since
+        processing has exceeded the timeout.
+        """
+        mock_process_files.delay = Mock()
+        mock_get_files.return_value = self.fake_reports
+
+        mock_started.return_value = self.today
+        mock_completed.return_value = None
+
+        mock_date.return_value = self.today + timedelta(hours=3)
+
+        get_report_files(**self.fake_get_report_args)
         mock_process_files.delay.assert_called()
+
+    @patch('masu.processor.tasks.ReportStatsDBAccessor.get_last_completed_datetime')
+    @patch('masu.processor.tasks.ReportStatsDBAccessor.get_last_started_datetime')
+    @patch('masu.processor.tasks._get_report_files')
+    @patch('masu.processor.tasks.process_report_file')
+    @patch('masu.external.date_accessor.DateAccessor.today')
+    def test_get_report_files_timestamps_empty_end_no_timeout(self,
+                                               mock_date,
+                                               mock_process_files,
+                                               mock_get_files,
+                                               mock_started,
+                                               mock_completed):
+        """
+        Test that the chained task is not called when no end time is set since
+        processing is in progress but completion timeout has not been reached.
+        """
+        mock_process_files.delay = Mock()
+        mock_get_files.return_value = self.fake_reports
+
+        mock_started.return_value = self.today
+        mock_completed.return_value = None
+
+        mock_date.return_value = self.today + timedelta(hours=1)
+
+        get_report_files(**self.fake_get_report_args)
+        mock_process_files.delay.assert_not_called()
 
     @patch('masu.processor.tasks.ReportStatsDBAccessor.get_last_completed_datetime')
     @patch('masu.processor.tasks.ReportStatsDBAccessor.get_last_started_datetime')

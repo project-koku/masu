ce54696b4e6ec1c0e9844a5e37a3733c15f35ca7
Chris Hambridge <chambrid@redhat.com>
2019-05-22 16:38:40 -0400

    Enable cloud watch logging.  (#450)
    * Enable cloud watch logging.
    
    * Add celery logger initialization.
    
    * Update per code review comments.

--- tests/celery/test_celery.py
+++ tests/celery/test_celery.py
@@ -19,10 +19,12 @@
 
 from celery import Celery
 from celery.app.task import Task
+from celery.utils.log import get_task_logger
 
 from masu import create_app
 from masu.celery import celery, update_celery_config
 from masu.config import Config
+from masu.util import setup_cloudwatch_logging
 from tests import MasuTestCase
 
 
@@ -58,3 +60,36 @@ class CeleryTest(MasuTestCase):
         self.assertNotEqual(test_celery.conf.imports, ())
         self.assertNotEqual(test_celery.conf.beat_schedule, {})
         self.assertNotEqual(repr(test_celery.Task), repr(Task))
+
+    def test_celery_functions_bogus_cw(self):
+        """Test that celery continues to function when
+        provided bogus AWS credentials for Cloud Watch logging.
+        """
+        test_celery = Celery(__name__, broker=Config.CELERY_BROKER_URL)
+        test_app = create_app(
+            {
+                'TESTING': True,
+                'SQLALCHEMY_TRACK_MODIFICATIONS': False,
+                'SQLALCHEMY_DATABASE_URI': 'sqlite:///test.db',
+                'SCHEDULE_REPORT_CHECKS': True,
+                'REMOVE_EXPIRED_REPORT_DATA_ON_DAY': '1',
+                'REMOVE_EXPIRED_REPORT_UTC_TIME': '00:00',
+                'CW_AWS_ACCESS_KEY_ID': 'bogusAWSkey',
+                'CW_AWS_SECRET_ACCESS_KEY': 'bogusAWSsecret'
+            }
+        )
+
+        self.assertIsNone(test_celery.conf.task_routes)
+        self.assertEqual(test_celery.conf.imports, ())
+        self.assertEqual(test_celery.conf.beat_schedule, {})
+        self.assertEqual(repr(test_celery.Task), repr(Task))
+
+        update_celery_config(test_celery, test_app)
+
+        self.assertIsNotNone(test_celery.conf.task_routes)
+        self.assertNotEqual(test_celery.conf.imports, ())
+        self.assertNotEqual(test_celery.conf.beat_schedule, {})
+        self.assertNotEqual(repr(test_celery.Task), repr(Task))
+        logger = get_task_logger('test')
+        setup_cloudwatch_logging(logger)
+        logger.info('log running')
